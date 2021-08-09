f662278b6f624bb63c48def1fe870cdeee5fc01d
root <root@git.maharshi.ninja>
2021-08-09 06:48:59 +0530

    Use fontsource for fonts

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import vuetify from './plugins/vuetify'
 import router from './router'
 
 import '@/styles/index.scss'
+import '@fontsource/ibm-plex-mono/latin-400.css'
+import '@fontsource/material-icons'
 
 Vue.config.productionTip = false
 
